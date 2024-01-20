205109a18a4696fd0f7f44dfe21b609d578987be
isaacxd <is.ascanoa@gmail.com>
2024-01-19 22:59:10 -0500

    new library:FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,25 @@
 import store
+#Libreria de fastapi
+from fastapi import FastAPI
+#Libreria para tener response de html
+from fastapi.responses import HTMLResponse
+
+#Nombre de la aplicacion para ejecutar
+app = FastAPI()
+
+#Funcion para obtener un listado en servidor
+@app.get('/')
+def get_list():
+    return [1,2,3,4,5]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contact():
+    return """
+        <h1>Hola soy una pagina en blanco</h1>
+        <p>Este es el primer paso en usar una libreria backend</p>
+    """
+    
+
 
 def run():
     store.get_categories()
